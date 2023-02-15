9a60a4bbac8926d2e7ea7d93626e26860a2e2eba
raopk <38057646+raopk@users.noreply.github.com>
2023-02-15 10:40:56 -0500

    Update 20230214_data_dictionary_all.py

--- 20230214_data_dictionary_all.py
+++ 20230214_data_dictionary_all.py
@@ -29,14 +29,14 @@ if selected_file != 'All':
 # Display the filtered dataframe
 st.dataframe(filtered_df)
 
-# Add a button to download the filtered data as an Excel file
-if not filtered_df.empty:
-    csv = filtered_df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="filtered_data.csv">Download Filtered Features as CSV</a>'
-    st.markdown(href, unsafe_allow_html=True)
-else:
-    st.warning("No data to download.")
+#Add a button to download the filtered data as an Excel file
+#if not filtered_df.empty:
+#    csv = filtered_df.to_csv(index=False)
+#   b64 = base64.b64encode(csv.encode()).decode()
+#    href = f'<a href="data:file/csv;base64,{b64}" download="filtered_data.csv">Download Filtered Features as CSV</a>'
+#    st.markdown(href, unsafe_allow_html=True)
+#else:
+#    st.warning("No data to download.")
 
 # Provide additional information or notes
-st.write("Note: This data dictionary is a work in progress.")
+st.write("Note: This data dictionary is a work in progress. Download filtered Data")
